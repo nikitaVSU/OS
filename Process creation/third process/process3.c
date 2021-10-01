d07e196a265bdce8dc55511c9389c8fd394a3f28
nikitaVSU <91758843+nikitaVSU@users.noreply.github.com>
2021-10-02 01:11:49 +0300

    Delete process3.c

--- Process creation/third process/process3.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h> 
-#include <sys/types.h> 
-#include <unistd.h> 
-int main() 
-{ 
-    printf("Hello from the child.c\n");
-    printf("PID of New process is: %d\n", getpid());
-    return 0; 
-}
